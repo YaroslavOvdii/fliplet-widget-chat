0e335cdcba23104c29a3e0d0e7e8e78c41a04d74
Yaroslav Ovdii <yaroslav.o@upplabs.com>
2020-11-16 17:04:47 +0200

    show chat in fullscreen on mobile devices

--- js/build.js
+++ js/build.js
@@ -62,6 +62,9 @@ Fliplet.Widget.instance('chat', function (data) {
   var clipboardjs;
   var autosizeInit = false;
   var isEditing = false;
+  var bodyPaddingTop = $('body').css('padding-top');
+  var hasTopMenu = $('body').hasClass('fl-with-top-menu');
+  var isMobile = $('html').hasClass('mobile');
   /** PHOTOSWIPE **/
   var photoswipeHtml = '<!-- Root element of PhotoSwipe. Must have class pswp. --> <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true"> <!-- Background of PhotoSwipe. It s a separate element as animating opacity is faster than rgba(). --> <div class="pswp__bg"></div> <!-- Slides wrapper with overflow:hidden. --> <div class="pswp__scroll-wrap"> <!-- Container that holds slides. PhotoSwipe keeps only 3 of them in the DOM to save memory. Don t modify these 3 pswp__item elements, data is added later on. --> <div class="pswp__container"> <div class="pswp__item"></div> <div class="pswp__item"></div> <div class="pswp__item"></div> </div> <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. --> <div class="pswp__ui pswp__ui--hidden"> <div class="pswp__top-bar"> <!-- Controls are self-explanatory. Order can be changed. --> <div class="pswp__counter"></div> <button class="pswp__button pswp__button--close" title="Close (Esc)"></button> <!-- <button class="pswp__button pswp__button--share" title="Share"> --> </button> <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button> <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button> <!-- Preloader demo http://codepen.io/dimsemenov/pen/yyBWoR --> <!-- element will get class pswp__preloader--active when preloader is running --> <div class="pswp__preloader"> <div class="pswp__preloader__icn"> <div class="pswp__preloader__cut"> <div class="pswp__preloader__donut"></div> </div> </div> </div> </div> <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap"> <div class="pswp__share-tooltip"></div> </div> <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"> </button> <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"> </button> <div class="pswp__caption"> <div class="pswp__caption__center"></div> </div> </div> </div> </div>';
   // Check if there's already
@@ -259,6 +262,12 @@ Fliplet.Widget.instance('chat', function (data) {
     $('.chat-card-holder[data-conversation-id="'+ conversationId +'"]').addClass('open');
   }
 
+  function showChatInFullScreen() {
+    if (hasTopMenu && isMobile) {
+      $wrapper.css('top', '-' + bodyPaddingTop);
+    }
+  }
+
   function openContacts() {
     // Reset to contacts list
     isViewingChannels = false;
@@ -356,6 +365,7 @@ Fliplet.Widget.instance('chat', function (data) {
 
   function closeConversation(clickedChat) {
     $wrapper.removeClass('chat-open');
+    $wrapper.css('top', '');
     $('.long-pressed').removeClass('long-pressed');
     $('.chat-area').removeClass('message-focused');
     $('.chat-card-holder').removeClass('open');
@@ -792,6 +802,8 @@ Fliplet.Widget.instance('chat', function (data) {
   }
 
   function createNewChatGroupSettings() {
+    showChatInFullScreen();
+
     if (isViewingChannels) {
       return joinPublicChannel();
     }
@@ -927,6 +939,8 @@ Fliplet.Widget.instance('chat', function (data) {
       })
       .on('click', '.chat-card-holder[data-conversation-id]', function() {
         if (allowClick) {
+          showChatInFullScreen();
+
           var id = $(this).data('conversation-id');
           var conversation = _.find(conversations, { id: id });
 
